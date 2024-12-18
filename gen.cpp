01a738151920da3872d40a4d524af1f5f39e4e33
Satvik Bhat <157195484+thesat7vik@users.noreply.github.com>
2024-12-18 16:08:32 +0530

    Create gen.cpp

---
+++ gen.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    return 0;
+}
